dcfbb5133849930cfdb6bb2860565db4e054769c
Mat <matze.krieger@googlemail.com>
2026-05-02 12:07:18 +0200

    added back-link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,39 @@
-"use client";
 
+import { seedUsers } from "@/lib/seed";
 import styles from "./page.module.css";
 import Link from "next/link";
+import { getUsers, insertUser } from "@/lib/actions";
 
-export default function MainPage() {
+export default async function MainPage() {
+  // seedUsers()
+
+  // const a = await insertUser(6, 'Peter', 15)
+
+  // const res = await getUsers()
+  // console.log(res)
+
   return (
     <div
       className={`${styles.page} ${styles.centerContent}`}
     >
-      <h1>- Snake-Game -</h1>
+      <h1>~ Snake-Game ~</h1>
+      <br/>
       <p>press the new game button to play</p>
       <Link href={"/game"} className={styles.buttonLink}>
         New Game
       </Link>
+      <br/>
+      <p>view the highscore list</p>
+      <Link href={"/highscore"} className={styles.buttonLink}>
+        Highscore
+      </Link>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <a href={"https://mk-dev.org/projects"} className={styles.buttonLink}>
+        Back
+      </a>
     </div>
   );
 }
