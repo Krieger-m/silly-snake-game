c0b3bc801c189239cc898ce9ca3807252dee6cb7
Mat <matze.krieger@googlemail.com>
2026-05-02 12:53:09 +0200

    edited pages

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,14 +1,13 @@
-
-
 import styles from "../page.module.css";
 import SnakeGame from "../_components/SnakeGame";
 import Link from "next/link";
+import { Spacing } from "../_components/Spacing";
 
 export default function GamePage() {
   return (
-    <div
-      className={`${styles.page} ${styles.centerContent}`}
-    >
+    <div className={`${styles.page} ${styles.centerContent}`}>
+      <h1>~ Snake-Game ~</h1>
+      <Spacing height={2} />
       <SnakeGame />
       <Link href={"/"} className={styles.buttonLink}>
         Back
--- src/app/highscore/page.tsx
+++ src/app/highscore/page.tsx
@@ -1,4 +1,3 @@
-
 import HighScoreList from "../_components/HighscoreList";
 import { Spacing } from "../_components/Spacing";
 import styles from "../page.module.css";
@@ -6,18 +5,16 @@ import Link from "next/link";
 
 export default function MainPage() {
   return (
-    <div
-      className={`${styles.page} ${styles.centerContent}`}
-    >
-      <h1>~ Snake-High-Score ~</h1>
-      <br/>
-      <HighScoreList/>
-      <br/>
+    <div className={`${styles.page} ${styles.centerContent}`}>
+      <h1>~ Snake-Game High-Score ~</h1>
+      <br />
+      <HighScoreList />
+      <br />
       <p>press the new game button to play</p>
       <Link href={"/game"} className={styles.buttonLink}>
         New Game
       </Link>
-<Spacing height={4}/>
+      <Spacing height={4} />
       <Link href={"/"} className={styles.buttonLink}>
         Back
       </Link>
